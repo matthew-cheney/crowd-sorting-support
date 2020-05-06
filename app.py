45fdbe908223ccfcfddd0df2463abceedb4bc449
Matthew Cheney <m.cheney95@outlook.com>
2020-05-06 10:20:16 -0600

    added startup prints

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 
+print('initializing app')
 app = Flask(__name__)
+
+print('defining routes')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -11,3 +14,5 @@ def home():
     else:
         print(f'or in {request.method}')
     return 'Hello, world!'
+
+print('done')
